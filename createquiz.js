96096dca8a1e6908bc63e57e4aab2b4e0a22fec3
Shreyansh Tiwari <127045990+shreyanshtiwari4@users.noreply.github.com>
2024-02-02 23:27:56 +0530

    Update createquiz.js
    gave user options to add different types of questions.

--- createquiz.js
+++ createquiz.js
@@ -1,290 +1,175 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyD0tMq6ZWGaJcG4VFWcmETbTxO7IOdDE3Q",
-    authDomain: "quizzer-a0c6e.firebaseapp.com",
-    databaseURL: "https://quizzer-a0c6e.firebaseio.com",
-    projectId: "quizzer-a0c6e",
-    storageBucket: "quizzer-a0c6e.appspot.com",
-    messagingSenderId: "235551414761",
-    appId: "1:235551414761:web:cb2e8ef047394bf666d378",
-    measurementId: "G-LHDCE10DHV"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+let quizDetailsForm = document.getElementById("quizDetailsForm");
+let sectionForm = document.getElementById("sectionForm");
+let quizForm = document.getElementById("questionForm");
+let sectionCounter = 0;
+let questionCounter = 0;
 
+function addQuizDetails() {
+    const quizName = document.getElementById("quizName").value;
+    const quizType = document.getElementById("quizName").value;
+    const quizTime = document.getElementById("quizName").value;
 
-firebase.auth().onAuthStateChanged(function (user) {
-    if (user) {
-
-    document.getElementById("nav-avatar").style.display = "block";
-    firebase.firestore().collection("User").doc(firebase.auth().getUid()).get().then(function (doc) {
-        document.getElementById("profile-name-avatar").innerHTML = doc.data().firstname + " " + doc.data().lastName;
-    });
-}
-});
-
-
-function logOut() {
-    firebase.auth().signOut();
-    window.location.href = "login_form.html";
-}
-
-
-let num_Of_questions = 0;
-
-
-
-function validation() {
-
-    let check = false;
-    let checkans = false;
-
-    if (num_Of_questions == 0) {
-        document.getElementById("error").innerHTML = "Please add Questions";
-    } else {
-
-
-        for (i = 1; i <= num_Of_questions; i++) {
-            let boolCheck = true;
-
-            let name = "question" + i;
-            var x = document.getElementById(name);
-            if (x != null) {
-
-                var z = x.getElementsByTagName('textarea');
-                var y = x.getElementsByTagName('input');
-                if (z[0].value.length == 0) {
-                    z[0].style.border = "solid red";
-                    check = true;
-                } else {
-                    z[0].style.border = "";
-                }
-
-                for (j = 1; j < y.length; j += 2) {
-
-                    if (y[j].value.length == 0) {
-                        y[j].style.border = "solid red";
-                        check = true;
-                    } else {
-                        y[j].style.border = "";
-                    }
-                }
-
-
-
-
-                for (k = 0; k < y.length; k += 2) {
-                    if (y[k].checked == true) {
-                        boolCheck = false;
-                    }
-                }
-
-                if (boolCheck) {
-                    checkans = true;
-                }
-
-            }
-        }
-
-        var na = document.getElementById("name_quiz");
-        if (na.value.length == 0) {
-            na.style.border = "solid red";
-            check = true;
-        } else {
-            na.style.border = "";
-        }
-
-
-
-        if (check && checkans) {
-            document.getElementById("error").innerHTML = "Please fill all data in red rectangle And Add correct answer";
-        } else if (check) {
-            document.getElementById("error").innerHTML = "Please fill all data in red rectangle";
-        } else if (checkans) {
-            document.getElementById("error").innerHTML = "Please add correct answer in all Questions";
-        } else {
-            document.getElementById("error").innerHTML = "";
-            SaveData();
-        }
-
-
-
-
-
+    if(quizName && quizTime && quizType) {
+        quizDetailsForm.style.display = "none";
+        sectionForm.style.display = "block";
+    }else{
+        alert("Please fill in all the details inorder to create a quiz.")
     }
 }
 
-function add_multi_Answer(name_question) {
+function addSection() {
+    const sectionName = document.getElementById("sectionName").value;
+    const questionType = document.getElementById("questionType").value;
 
-    let num_question = document.getElementById(name_question);
-    let div_of_questions = num_question.getElementsByTagName("div")[0];
-    let num_Of_Answer = num_question.getElementsByTagName("p")[0].innerHTML;
+    if(sectionName && questionType){
+        document.getElementById("sectionForm").style.display = "none";
+        sectionCounter++;
+        // console.log(sectionName);
+        // console.log(sectionCounter);
+        let sectionDiv = document.createElement("div");
+        sectionDiv.id = `section-${sectionCounter}`;
+        sectionDiv.innerHTML = `<h3>Section ${sectionCounter}: ${sectionName}</h3>`;
 
+        let questionForm = document.createElement("form");
+        questionForm.id = `questionForm-${sectionCounter}`;
 
+        let questionsContainer = document.createElement("div");
+        questionsContainer.id = `questionsContainer-${sectionCounter}`;
+        questionForm.appendChild(questionsContainer);
 
-    var new_answer = div_of_questions.getElementsByTagName("div")[0];
+        let addQuestionButton = document.createElement("button");
+        addQuestionButton.type = "button";
+        addQuestionButton.textContent = "Add Question";
+        addQuestionButton.onclick = function() {
+            addQuestion(sectionCounter, questionType);
+        };
+        questionForm.appendChild(addQuestionButton);
+        
+        let submitSectionButton = document.createElement("button");
+        submitSectionButton.type = "button";
+        submitSectionButton.textContent = "Submit Section";
+        submitSectionButton.onclick = function() {
+            submitSection(sectionCounter, sectionName);
+        };
+        submitSectionButton.style = "none";
+        questionForm.appendChild(submitSectionButton);
 
-    var cln = new_answer.cloneNode(true);
-    cln.getElementsByTagName('input')[0].checked = false;
-    cln.getElementsByTagName('input')[1].value = "";
+        sectionDiv.appendChild(questionForm);
 
-    console.log(cln);
-    div_of_questions.appendChild(cln);
+        document.getElementById("quizForm").appendChild(sectionDiv);
 
-
-    num_Of_Answer++;
-    console.log(num_Of_Answer);
-    num_question.getElementsByTagName("p")[0].innerHTML = num_Of_Answer;
-}
-
-function add_one_Answer(name_question) {
-    let num_question = document.getElementById(name_question);
-    let div_of_questions = num_question.getElementsByTagName("div")[0];
-    let num_Of_Answer = num_question.getElementsByTagName("p")[0].innerHTML;
-
-
-
-    var new_answer = div_of_questions.getElementsByTagName("div")[0];
-
-    var cln = new_answer.cloneNode(true);
-    cln.getElementsByTagName('input')[0].checked = false;
-    cln.getElementsByTagName('input')[1].value = "";
-
-    console.log(cln);
-    div_of_questions.appendChild(cln);
-
-
-    num_Of_Answer++;
-    console.log(num_Of_Answer);
-    num_question.getElementsByTagName("p")[0].innerHTML = num_Of_Answer;
-}
-
-function delete_answer(name_question) {
-    let num_question = document.getElementById(name_question);
-    let num_Of_Answer = num_question.getElementsByTagName("p")[0].innerHTML;
-    if (num_Of_Answer > 3) {
-        num_Of_Answer--;
-        console.log(num_Of_Answer);
-        num_question.getElementsByTagName("p")[0].innerHTML = num_Of_Answer;
-        return true;
+        // document.getElementById("questionForm").style.display = "block";
+    }else{
+        alert("Please fill in all the details to create a new section.");
     }
 }
+function addQuestion(sectionId, questionType) {
+    questionCounter++;
 
-function delete_one_answer(name_question) {
-    let num_question = document.getElementById(name_question);
-    let num_Of_Answer = num_question.getElementsByTagName("p")[0].innerHTML;
-    if (num_Of_Answer > 2) {
-        num_Of_Answer--;
-        console.log(num_Of_Answer);
-        num_question.getElementsByTagName("p")[0].innerHTML = num_Of_Answer;
-        return true;
-    }
-}
+    // const questionType = document.getElementById("questionType").value;
+    let questionsContainer = document.getElementById(`questionsContainer-${sectionId}`); //here the labels and inputs will be added based on the type of question
 
-function add_multi_question() {
-    num_Of_questions++;
-    var str = "<div class=\"p-2 flex-fill text-center Qusistion\" id=\"question" + num_Of_questions + "\"><label for=\"uname\">Quistion " + num_Of_questions + "</label><button type=\"button\" class=\"closeButton\" onclick=\"this.parentNode.remove();\" style=\"float: right\">X</button><textarea class=\"form-control textA\" rows='7' name=\"text\" required></textarea><br /><p class=\"collapse\">3</p><div><div class=\"form-group form-check\"><input type=\"checkbox\" class=\"form-radio\"><input type=\"text\" class=\"form-control-sm styleinput\"><button type=\"button\" class=\"closeButton\" onclick=\"if(delete_answer('question" + num_Of_questions + "')){this.parentNode.remove();}\">X</button></div><div class=\"form-group form-check\"><input type=\"checkbox\" class=\"form-radio\"><input type=\"text\" class=\"form-control-sm styleinput\"><button type=\"button\" class=\"closeButton\" onclick=\"if(delete_answer('question" + num_Of_questions + "')){this.parentNode.remove();}\">X</button></div><div class=\"form-group form-check\"><input type=\"checkbox\" class=\"form-radio\"><input type=\"text\" class=\"form-control-sm styleinput\"><button type=\"button\" class=\"closeButton\" onclick=\"if(delete_answer('question" + num_Of_questions + "')){this.parentNode.remove();}\">X</button></div></div><button type=\"button\" onclick=\"add_multi_Answer('question" + num_Of_questions + "')\" class=\"btn btn-info\" style=\"width: 20%\">Add answer</button></div>";
+    let questionDiv = document.createElement("div");
+    questionDiv.className = "questionDiv";
 
+    let questionLabel = document.createElement("label");
+    questionLabel.innerHTML = `Question ${questionCounter}:`;
 
-    var child = document.createElement('div');
-    child.innerHTML = str;
-    document.getElementById('questions').appendChild(child);
-}
+    let questionInput = document.createElement("input");
+    questionInput.type = "text";
+    questionInput.name = `question-${sectionId}-${questionCounter}:`;
 
-function add_one_question() {
-    num_Of_questions++;
-    var str = "<div class=\"p-2 flex-fill text-center Qusistion\" id=\"question" + num_Of_questions + "\"><label for=\"uname\">Quistion " + num_Of_questions + "</label><button type=\"button\" class=\"closeButton\" onclick=\"this.parentNode.remove();\" style=\"float: right\">X</button><textarea class=\"form-control textA\" rows=\"7\" name=\"text\" required></textarea><br /><p class=\"collapse\">2</p><div><div class=\"form-group form-check\"><input type=\"radio\" name=\"q" + num_Of_questions + "\" class=\"form-radio\"><input type=\"text\" class=\"form-control-sm styleinput\"><button type=\"button\" class=\"closeButton\" onclick=\"if(delete_one_answer('question" + num_Of_questions + "')){this.parentNode.remove();}\">X</button></div><div class=\"form-group form-check\"><input type=\"radio\" name=\"q" + num_Of_questions + "\" class=\"form-radio\"><input type=\"text\" class=\"form-control-sm styleinput\"><button type=\"button\" class=\"closeButton\" onclick=\"if(delete_one_answer('question" + num_Of_questions + "')){this.parentNode.remove();}\">X</button></div></div><button type=\"button\" onclick=\"add_one_Answer('question" + num_Of_questions + "')\" class=\"btn btn-info\" style=\"width: 20%\">Add answer</button></div>";
+    // Add the 'for' attribute to associate the label with the input field
+    questionLabel.setAttribute("for", `question-${sectionId}-${questionCounter}`);
 
+    questionDiv.appendChild(questionLabel);
+    questionDiv.appendChild(questionInput);
 
-    var child = document.createElement('div');
-    child.innerHTML = str;
-    document.getElementById('questions').appendChild(child);
-}
-
-
-function SaveData() {
-    var number_of_Q = 0;
-    var firestore = firebase.firestore();
-
-    var access = makeid();
-
-
-    firebase.auth().onAuthStateChanged((user) => {
-        if (user) {
-            var arra = new Array();
-            var name_quiz = document.getElementById("name_quiz").value;
-            var time_quiz = document.getElementById("time_quiz").value;
-            var point_quiz = document.getElementById("point").value;
-
-
-            const docRef = firestore.collection("Quiz").doc(access);
-            docRef.set({
-                name: name_quiz,
-                Time: time_quiz,
-                Points: point_quiz,
-                Instructor: user.uid,
-                enrolled: arra
-            });
-
-            var docRef5 = firestore.collection("User").doc(user.uid);
-            docRef5.update({
-                instQuizzes: firebase.firestore.FieldValue.arrayUnion(access)
-            });
-
-            //            console.log(user.uid);
-        } else {
-            // User not logged in or has just logged out.
+    if (questionType === "MCQ") {
+       // Add options and radio buttons
+        for (let i = 0; i < 4; i++) {
+            let optionLabel = document.createElement("label");
+            optionLabel.innerHTML = `Option ${i + 1}:`;
+            let optionInput = document.createElement("input");
+            optionInput.type = "text";
+            optionInput.name = `option-${sectionId}-${questionCounter}-${i + 1}`;
+            let optionRadio = document.createElement("input");
+            optionRadio.type = "radio";
+            optionRadio.name = `correct-answer-${sectionId}-${questionCounter}`;
+            optionLabel.appendChild(optionInput);
+            optionLabel.appendChild(optionRadio);
+            questionDiv.appendChild(optionLabel);
         }
-    });
+    }else if (questionType === "ShortAnswer") {
+         
+        let answerLabel = document.createElement("label");
+        answerLabel.innerHTML = "Correct Answer:";
 
+        let answerInput = document.createElement("input");
+        answerInput.type = "text";
+        answerInput.name = `answer-${sectionId}-${questionCounter}`;
 
-
-    for (i = 1; i <= num_Of_questions; i++) {
-        let correct = new Array();
-        let all = new Array();
-
-        let name = "question" + i;
-        var x = document.getElementById(name);
-        if (x != null) {
-            number_of_Q++;
-            var text_Question = x.getElementsByTagName('textarea')[0].value;
-            var y = x.getElementsByTagName('input');
-
-            for (k = 0; k < y.length; k += 2) {
-                if (y[k].checked == true) {
-                    correct.push(y[k + 1].value);
-                }
-                all.push(y[k + 1].value);
-            }
-
-
-            var n = "Question" + number_of_Q;
-            const docRef1 = firestore.collection("Quiz").doc(access).collection("Questions").doc(n);
-            docRef1.set({
-                QuestionText: text_Question,
-                All: all,
-                Correct: correct
-            });
-        }
+        questionDiv.appendChild(answerLabel);
+        questionDiv.appendChild(answerInput);
     }
+    else if (questionType === "truefalse"){
+        let answerLabel = document.createElement("label");
+        answerLabel.innerHTML = "Correct Answer:";
 
+        let answerInput = document.createElement("input");
+        answerInput.type = "text";
+        answerInput.name = `answer-${sectionId}-${questionCounter}`;
 
-    document.getElementById("b1").style.display = "none";
-    document.getElementById("b2").style.display = "none";
-    document.getElementById("b3").style.display = "none";
+        questionDiv.appendChild(answerLabel);
+        questionDiv.appendChild(answerInput);
+    
+    }else if (questionType === "LongAnswer") {
+         
+        let answerLabel = document.createElement("label");
+        answerLabel.innerHTML = "Correct Answer:";
 
-    var cd = "<h1>The Access Code is :  " + access + "</h1><br><button class=\"btn btn-primary\" onclick=\"window.location.href = 'profile.html';\" style=\"width: 40%;\">go to profile</button>";
+        let answerInput = document.createElement("input");
+        answerInput.type = "text";
+        answerInput.name = `answer-${sectionId}-${questionCounter}`;
 
-    document.getElementById("questions").innerHTML = cd;
+        questionDiv.appendChild(answerLabel);
+        questionDiv.appendChild(answerInput);
+    }
+    
+    questionsContainer.appendChild(questionDiv);
 
+    // Show submit section button after adding the first question
+    if (questionCounter === 1) {
+        submitSectionButton.style = "block"
+    }
 }
 
+function submitSection(sectionId, sectionName) {
+    // Hide the current question form
+    document.getElementById(`questionForm-${sectionId}`).style.display = "none";
 
-function makeid() {
-    var result = '';
-    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var charactersLength = characters.length;
-    for (var i = 0; i < 10; i++) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
+    // Display the newly created section
+    const sectionSummary = document.createElement("div");
+    sectionSummary.textContent = `Section ${sectionId}: ${sectionName}`;
+    sectionSummary.style.border = "1px solid black"; // Add border for visualization
+
+    const addSectionButton = document.createElement("button");
+    addSectionButton.textContent = "Add New Section";
+    addSectionButton.onclick = function() {
+        document.getElementById("sectionForm").style.display = "block";
+    };
+    
+    const submitQuizButton = document.createElement("button");
+    submitQuizButton.textContent = "Submit Quiz";
+    // Append section summary and buttons to the quiz form
+    document.getElementById("quizForm").appendChild(sectionSummary);
+    document.getElementById("quizForm").appendChild(addSectionButton);
+    document.getElementById("quizForm").appendChild(submitQuizButton);
+
+    // Reset questionCounter and clear the form
+    questionCounter = 0;
+    document.getElementById(`questionsContainer-${sectionId}`).innerHTML = "";
+
+    // Alert the user
+    alert(`Section ${sectionId} submitted successfully!`);
 }
